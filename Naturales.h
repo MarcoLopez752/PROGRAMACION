8d722fc53195418cbe04968671612edac8fdd566
MarcoLopez752 <42274053+MarcoLopez752@users.noreply.github.com>
2019-02-25 20:13:15 -0400

    Update Naturales.h

--- Naturales.h
+++ Naturales.h
@@ -1,220 +1,343 @@
 #pragma once
+//1,2,3,4,6,8,
 
+	#include "stdafx.h"
+	#include <iostream>
+	#include "conio.h"
+	#include "math.h"
+	#include <stdlib.h>
+	#include "Naturales.h"
+	#include <time.h>
 
-#include "stdafx.h"
-#include <iostream>
-#include "conio.h"
-#include "math.h"
-#include <stdlib.h>
-#include "Naturales.h"
-#include <time.h>
+	using namespace std;
 
-using namespace std;
-
-class Naturales
-{
-private:
-
-	int entero;
-
-public:
-
-	int get_entero()
+	class Naturales
 	{
-		return entero;
-	}
+	private:
 
-	void set_entero(int e)
-	{
-		entero = e;
-	}
+		int entero, insertar;
+		int a[50], b[50];
 
-	void Leer()
-	{
-		cout << "Ingrese el numero a unir" << endl;
-		cin >> entero;
-	}
+	public:
 
-	void  Escuchar()
-	{
-		cin >> entero;
-	}
-
-	void Mostrar()
-
-	{
-		cout << "La union de digito es: " << entero << endl;
-	}
-
-	void Ver()
-	{
-		cout << entero << endl;
-	}
-
-	bool Mayor(Naturales y)
-	{
-		if (y.entero < entero)
+		int get_entero()
 		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	int Contar_digitos()
-	{
-		int i = entero;
-		int contar = 0;
-		while (i > 0)
-		{
-			i = i / 10;
-			contar = contar + 1;
+			return entero;
 		}
 
-		return contar;
-
-	}
-
-	Naturales Unir(Naturales y)
-	{
-		Naturales c;
-		int i = entero;
-		int conta = y.Contar_digitos();
-
-		while (conta > 0)
+		void set_entero(int e)
 		{
-			i = i * 10;
-			conta = conta - 1;
-
+			entero = e;
 		}
 
-		i = i + y.entero;
-		c.entero = i;
-		return c;
-	}
-	int Invertir()
-	{
-		int a = 0;
-
-		while (entero != 0)
+		void Leer()
 		{
-
-			a = (a * 10) + (a % 10);
-			a = a / 10;
-
-		}
-
-		return a;
-
-	}
-
-	int Random()
-	{
-		srand(time(NULL));
-		entero = 1 + rand() % (100);
-		return entero;
-
-	}
-
-
-	int Adivinanza(Naturales Dato)
-	{
-		do
-		{
-			cout << "Digite un numero: ";
+			cout << "Ingrese el numero a unir" << endl;
 			cin >> entero;
-			if (entero > Dato.entero)
-			{
-				cout << "Digite un numero menor" << endl;
-			}
-			if (entero < Dato.entero)
-			{
-				cout << "Digite un numero mayor" << endl;
-			}
 		}
 
-		while (entero != Dato.entero);
-
-		cout << "Felicidades adivinaste el numero" << endl;
-		return 0;
-	}
-
-	int Elevar()
-	{
-		entero = entero * 2;
-		return entero;
-	}
-
-	int invertir(int I)
-	{
-		int v, a = I, n = 0;
-		while (a > 0)
-		{						
-			n = n * 10 + a % 10;
-			a = a / 10;
-		}
-		return n;
-	}
-
-
-
-	Naturales Palindromo()
-	{
-		Naturales a;
-		int b;
-		b = invertir(entero);
-		a.entero = b;
-		return a;
-	}
-
-	bool Ciclo(int a, int b) //repeticion//
-	{
-		bool cont = false;
-		int c = 0;
-		while (a > 0 && c<2)
+		void  Escuchar()
 		{
-			if (b == a % 10)
-			{
-				c=c+1;
-			}
-			if (c > 1)
-			{
-				cont = true;
-				break;
-			}
-			a = a / 10;
+			cin >> entero;
 		}
-		return cont;
-	}
 
-	int guardar(int n, int d)
-	{
-		int x = n, y = 0, v = 1;
-		while (x > 0) {
-			if (d != x % 10)
-			{
-				y = y + (x % 10) * v;
-				v = v * 10;
-			}
-			x = x / 10;
-		}
-		return y;
-	}
+		void Mostrar()
 
-	Naturales No_Repetidos()
-	{
-		Naturales y;
-		for (int i = 0; i < 10; i++)
 		{
-			if (Ciclo(entero, i))
+			cout << "La union de digito es: " << entero << endl;
+		}
+
+		void Ver()
+		{
+			cout << entero << endl;
+		}
+
+		bool Mayor(Naturales y)
+		{
+			if (y.entero < entero)
 			{
-				entero = guardar(entero, i);
+				return true;
+			}
+			else
+			{
+				return false;
 			}
 		}
-		y.entero = entero;
-		return y;
-	}
 
-};
+		int Contar_digitos()
+		{
+			int i = entero;
+			int contar = 0;
+			while (i > 0)
+			{
+				i = i / 10;
+				contar = contar + 1;
+			}
+
+			return contar;
+
+		}
+
+		Naturales Unir(Naturales y)
+		{
+			Naturales c;
+			int i = entero;
+			int conta = y.Contar_digitos();
+
+			while (conta > 0)
+			{
+				i = i * 10;
+				conta = conta - 1;
+
+			}
+
+			i = i + y.entero;
+			c.entero = i;
+			return c;
+		}
+		int Invertir()
+		{
+			int a = 0;
+
+			while (entero != 0)
+			{
+
+				a = (a * 10) + (a % 10);
+				a = a / 10;
+
+			}
+
+			return a;
+
+		}
+
+		int Random()
+		{
+			srand(time(NULL));
+			entero = 1 + rand() % (100);
+			return entero;
+
+		}
+
+
+		int Adivinanza(Naturales Dato)
+		{
+			do
+			{
+				cout << "Digite un numero: ";
+				cin >> entero;
+				if (entero > Dato.entero)
+				{
+					cout << "Digite un numero menor" << endl;
+				}
+				if (entero < Dato.entero)
+				{
+					cout << "Digite un numero mayor" << endl;
+				}
+			}
+
+			while (entero != Dato.entero);
+
+			cout << "Felicidades adivinaste el numero" << endl;
+			return 0;
+		}
+
+		int Elevar()
+		{
+			entero = entero * 2;
+			return entero;
+		}
+
+		int invertir(int I)
+		{
+			int v, a = I, n = 0;
+			while (a > 0)
+			{						
+				n = n * 10 + a % 10;
+				a = a / 10;
+			}
+			return n;
+		}
+
+
+
+		Naturales Palindromo()
+		{
+			Naturales a;
+			int b;
+			b = invertir(entero);
+			a.entero = b;
+			return a;
+		}
+
+		bool Ciclo(int a, int b) //repeticion//
+		{
+			bool cont = false;
+			int c = 0;
+			while (a > 0 && c<2)
+			{
+				if (b == a % 10)
+				{
+					c=c+1;
+				}
+				if (c > 1)
+				{
+					cont = true;
+					break;
+				}
+				a = a / 10;
+			}
+			return cont;
+		}
+
+		int guardar(int n, int d)
+		{
+			int x = n, y = 0, v = 1;
+			while (x > 0) {
+				if (d != x % 10)
+				{
+					y = y + (x % 10) * v;
+					v = v * 10;
+				}
+				x = x / 10;
+			}
+			return y;
+		}
+
+		Naturales No_Repetidos()
+		{
+			Naturales y;
+			for (int i = 0; i < 10; i++)
+			{
+				if (Ciclo(entero, i))
+				{
+					entero = guardar(entero, i);
+				}
+			}
+			y.entero = entero;
+			return y;
+		}
+
+		int Mayor(int a)
+		{
+			int i = -1, c = a;
+			while (c > 0)
+			{
+				if (i < c % 10)
+				{
+					i = c % 10;
+					if (i == 9) break;
+				}
+				c = c / 10;
+			}
+			return i;
+		}
+
+		int Selector(int a, int b)
+		{
+			int f = a, x = 0, y = 1;
+			while (entero > 0) {
+				if (b != entero % 10)
+				{
+					x = x + (entero % 10) * y;
+					y = y * 10;
+				}
+				entero = entero / 10;
+			}
+			return x;
+		}
+
+		Naturales Eliminar()
+		{
+			Naturales a;
+			a.entero = Selector(entero, Mayor(entero));
+			return a;
+		}
+
+		void LeerD()
+		{
+			cin >> entero;
+			cin >> insertar;
+		}
+		Naturales Insertar()
+		{
+			Naturales r;
+			int x = entero, y = insertar, d, a = 0, b = 1;
+			int contador = 0;
+		
+				while (x > 0 || contador==0)
+				{
+					d = x % 10;
+					if ((y > d && contador==0) || x == 0)
+					{
+						a = a + y * b;
+						b = b * 10;
+						contador = 1;
+					}
+					a = a + d * b;
+					x = x / 10;
+					b = b * 10;
+				}
+			
+			r.entero = a;
+			return r;
+		}
+		
+		
+		void LeerHoras()
+		{	
+			cin >> entero;
+			for (int i = 0; i < entero; i++)
+			{
+				cout << "Horas trabajadas. Interno " << i + 1<<": " << endl;
+				cin >> a[i];
+
+			}
+		}
+		
+
+		void mostrar()
+		{
+			for (int i = 0; i < entero; i++)
+			{
+				b[i] = a[i] * 20;
+			}
+			
+			cout << " ____________________________________________________________"<<endl;
+			std::cout << "|-------------------Planilla de sueldos----------------------|" << std::endl;
+			std::cout << "|No                   Horas de trabajo                 Sueldo|" << std::endl;
+			std::cout << "|------------------------------------------------------------|" << std::endl;
+			for (int i = 0; i < entero; i++)
+			{
+				std::cout <<"|"<< i + 1 << "                           " << a[i] << "h                         " << b[i] << "bs |" << std::endl;
+				
+			}
+			cout << "|____________________________________________________________|";
+		}
+
+		void Automorfico()
+		{
+			int p, n, d, c;
+			int contador = 0;
+			int cont=0; 
+			while (contador == 0)
+			{
+				p = entero * entero;
+				n = entero;
+				d = Contar_digitos();
+				c = pow(10, d);
+				if (p%c == n)
+				{
+					mostrar();
+				}
+				entero++;
+				cont++;
+				if (cont == 1000)
+				{
+					contador = 1;
+				}
+			}
+
+
+		}
+	};
